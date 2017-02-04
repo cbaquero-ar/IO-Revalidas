45ecbb2bdd76f1c904fb42c475f123f4c1d41c36
AnaCarolina <baquero.carolina@gmail.com>
2017-02-04 20:12:43 -0300

    Cambios sabado 4/2
    documentacion del procedimiento y scripts de OneR andando y J48 sin
    probar

--- Desarrollo/R Scripts/QueryDB.R
+++
@@ -1,4 +0,0 @@
-# query the data from postgreSQL 
-df_postgres <- dbGetQuery(con, "SELECT * from revalidas")
-df_postgres [ , ]
-str(df_postgres)
---
+++ Desarrollo/R Scripts/QueryOneR.R
@@ -0,0 +1,15 @@
+# query the data from postgreSQL 
+#df_postgres <- dbGetQuery(con, "SELECT anio_ingreso,finales_adeudados,cursadas_adeudadas,optativas_adeudadas,adeuda_trab_final_inicio,plan,tutor_r1, recibido_fin_r1 from revalidas WHERE usable=TRUE")
+df_postgres <- dbGetQuery(con, "SELECT finales_adeudados,cursadas_adeudadas,optativas_adeudadas,adeuda_trab_final_inicio,plan,tutor_r1, recibido_fin_r1 from revalidas WHERE usable=TRUE")
+
+df_postgres [ , ]
+str(df_postgres)
+
+#install.packages(OneR)
+require("OneR")
+model <- OneR(df_postgres, verbose = TRUE)
+summary(model)
+plot(model)
+prediction <- predict(model, df_postgres)
+eval_model(prediction, df_postgres)
+
