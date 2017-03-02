1304b9a0987997722784143260218a77c19993f9
AnaCarolina <baquero.carolina@gmail.com>
2017-03-02 20:57:03 -0300

    Doc Procedimientos y script J48
    Doc Procedimientos actualziadon¡ con los resultados de los datos bien
    formados al 07/02/2017. Script QueryJ48actualizado con el contenido de
    j48.r script

--- Desarrollo/R Scripts/QueryJ48.R
+++ Desarrollo/R Scripts/QueryJ48.R
@@ -1,1 +1,24 @@
-w <- read.arff(system.file("arff","weather.nominal.arff", package = "RWeka")) ## Identify a decision tree. m <- J48(play~., data = w) m ## Use 10 fold cross-validation. e <- evaluate_Weka_classifier(m, cost = matrix(c(0,2,1,0), ncol = 2), numFolds = 10, complexity = TRUE, seed = 123, class = TRUE) e summary(e) e$details 
+## install.packages("RWeka")
+require("RWeka")
+
+df_postgres <- dbGetQuery(con, "SELECT finales_adeudados,
+    									cursadas_adeudadas,
+									    optativas_adeudadas,
+									    adeuda_trab_final_inicio ,
+									    plan,
+									    tutor_r1,
+									    tutor_r2,
+										recibido_fin_r1
+									from revalidas")
+
+## Identify a decision tree.
+df_postgres <- lapply(df_postgres, as.factor)
+m <- J48(recibido_fin_r1 ~., data = df_postgres, control= Weka_control(M=1,U=TRUE))
+
+m
+
+## Use 10 fold cross-validation.
+e <- evaluate_Weka_classifier(m, cost = matrix(c(0,2,1,0), ncol = 2),numFolds = 10, complexity = TRUE,seed = 123, class = TRUE)
+e
+summary(e)
+e$details
