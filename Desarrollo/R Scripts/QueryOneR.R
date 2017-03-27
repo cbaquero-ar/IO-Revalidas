135285e084f1103c367232562a547a5a1a78b381
JuanJoseRodriguez <jjoserodriguez@live.com>
2017-03-27 20:19:40 -0300

    Se modifico el select con los campos fecha
    Se modifico el select con los campos fecha

--- Desarrollo/R Scripts/QueryOneR.R
+++ Desarrollo/R Scripts/QueryOneR.R
@@ -1,6 +1,14 @@
 # query the data from postgreSQL 
 #df_postgres <- dbGetQuery(con, "SELECT anio_ingreso,finales_adeudados,cursadas_adeudadas,optativas_adeudadas,adeuda_trab_final_inicio,plan,tutor_r1, recibido_fin_r1 from revalidas WHERE usable=TRUE")
-df_postgres <- dbGetQuery(con, "SELECT finales_adeudados,cursadas_adeudadas,optativas_adeudadas,adeuda_trab_final_inicio,plan,tutor_r1, recibido_fin_r1 from revalidas WHERE usable=TRUE")
+df_postgres <- dbGetQuery(con, "SELECT  fecha_inscripcion,
+										finales_adeudados,
+										cursadas_adeudadas,
+										optativas_adeudadas,
+										adeuda_trab_final_inicio,
+										recibido,
+										fecha_recibido 
+								from revalidas 
+								WHERE usable=TRUE")
 
 df_postgres [ , ]
 str(df_postgres)
@@ -12,4 +20,3 @@ summary(model)
 plot(model)
 prediction <- predict(model, df_postgres)
 eval_model(prediction, df_postgres)
-
